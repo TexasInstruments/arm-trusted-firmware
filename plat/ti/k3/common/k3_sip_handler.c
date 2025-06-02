TexasInstruments/arm-trusted-firmware

commit 7a76ef2f283d505b7d32bfd35b259cdcdd9c1869

PENDING: feat(ti): add keywriter lite SMC handler
Add keywriter lite SMC call handler.
Add ti_fuse_writebuff_handler and call ti_sci_keywriter_lite
function accordingly.

Change-Id: Ie95b7456c4882881508d4ff77e03c4a3b16b853d
Signed-off-by: Harsha Vardhan V M <h-vm@ti.com>

plat/ti/k3/common/k3_sip_handler.c
@@ -1,0 +1,50 @@
+/*
+ * Copyright (c) 2025, Texas Instruments. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <common/debug.h>
+#include <lib/smccc.h>
+#include <k3_sip_svc.h>
+#include <ti_sci.h>
+#include <ti_sci_protocol.h>
+
+/* K3 FUSE BUFFER STRUCT */
+struct k3_fuse_writebuff {
+	uint32_t ver_info;
+	uint32_t tisci_id;
+	void *payload;
+} __packed;
+
+/**
+ * ti_fuse_writebuiff_handler - Handler for fuse writebuff SMCCC.
+ *
+ * @x1: The starting memory address of the buffer
+ *
+ * Return: 0 if all goes well, else appropriate error message
+ */
+int ti_fuse_writebuff_handler(u_register_t x1)
+{
+	int ret;
+	const struct k3_fuse_writebuff *k3_fuse_buff;
+
+	if (x1 == 0U) {
+		ERROR("x1 param is NULL\n");
+		return SMC_UNK;
+	}
+	k3_fuse_buff = (const struct k3_fuse_writebuff *)x1;
+	NOTICE("Writebuff version info: 0x%x\n", k3_fuse_buff->ver_info);
+	if (k3_fuse_buff->tisci_id == TISCI_MSG_KEY_WRITER_LITE) {
+		ret = ti_sci_keywriter_lite((unsigned long)&k3_fuse_buff->payload);
+		if (ret) {
+			ERROR("Keywriter Lite Failed: (%d)\n", ret);
+			return SMC_UNK;
+		}
+	} else {
+		ERROR("Invalid TISCI ID (0x%x)\n", k3_fuse_buff->tisci_id);
+		return SMC_UNK;
+	}
+
+	return 0;
+}

plat/ti/k3/board/am62l/board.mk
@@ -103,6 +103,7 @@ PLAT_INCLUDES += -Iplat/ti/k3/board/${TARGET_BOARD}/pm			\
 
 BL31_SOURCES		+=	\
 				${PLAT_PATH}/common/k3_svc.c		\
+				${PLAT_PATH}/common/k3_sip_handler.c		\
 				drivers/scmi-msg/base.c			\
 				drivers/scmi-msg/entry.c			\
 				drivers/scmi-msg/smt.c			\

plat/ti/k3/common/k3_svc.c
@@ -53,6 +53,9 @@ uintptr_t sip_smc_handler(uint32_t smc_fid,
 		    scmi_smt_fastcall_smc_entry(0);
 		    SMC_RET1(handle, 0);
 
+	    case K3_SIP_OTP_WRITEBUFF:
+		    SMC_RET1(handle, ti_fuse_writebuff_handler(x1));
+
 	    default:
 		    ERROR("%s: unhandled SMC (0x%x)\n", __func__, smc_fid);
 		    SMC_RET1(handle, SMC_UNK);

plat/ti/k3/include/k3_sip_svc.h
@@ -1,13 +1,29 @@
+/*
+ * Copyright (c) 2025, Texas Instruments. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef K3_SIP_SVC_H
+#define K3_SIP_SVC_H
+
+#include <stdint.h>
+
 /* SMC function IDs for SiP Service queries */
 #define SIP_SVC_CALL_COUNT      0x8200ff00
 #define SIP_SVC_UID         0x8200ff01
 #define SIP_SVC_VERSION         0x8200ff03
 #define K3_SIP_SCMI_AGENT0      0x82004000
+#define K3_SIP_OTP_WRITEBUFF    0xC2000000
 
 /* TI SiP Service Calls version numbers */
 #define K3_SIP_SVC_VERSION_MAJOR    0x0
 #define K3_SIP_SVC_VERSION_MINOR    0x1
 
 /* Number of TI SiP Calls implemented */
-#define K3_COMMON_SIP_NUM_CALLS     0x3
+#define K3_COMMON_SIP_NUM_CALLS     0x4
 
+/* TI Fuse writebuff SMC handler */
+int ti_fuse_writebuff_handler(u_register_t x1);
+
+#endif /* K3_SIP_SVC_H */

commit b7873cd7b669df9c7335f5ced45880e23eb0164d

PENDING: fix(ti): Flush OTP memory blob before handoff
The OTP blob is ultimately consumed by the secure co-processor in the
system and thus needs to be flushed to the point of cache coherency.
These blobs are at most a couple of KBs in size, but flush the entire
PAGE to be on the safer side.

Without this change, the responsibility for flushing the cache for the
blob being passed lies with the caller (like U-Boot), which isn't the
right place since U-Boot has no knowledge of the backend implementation
of the SVC call.

Fixes: 7a76ef2f283d ("PENDING: feat(ti): add keywriter lite SMC handler")
Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

plat/ti/k3/common/k3_sip_handler.c
@@ -4,6 +4,9 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <platform_def.h>
+
+#include <arch_helpers.h>
 #include <common/debug.h>
 #include <lib/smccc.h>
 #include <k3_sip_svc.h>
@@ -36,6 +39,13 @@ int ti_fuse_writebuff_handler(u_register_t x1)
 	k3_fuse_buff = (const struct k3_fuse_writebuff *)x1;
 	NOTICE("Writebuff version info: 0x%x\n", k3_fuse_buff->ver_info);
 	if (k3_fuse_buff->tisci_id == TISCI_MSG_KEY_WRITER_LITE) {
+
+		/*
+		 * Buffer needs to be visible to the TISCI co-processor, since
+		 * the size is unknown, flush the entire page
+		 */
+		flush_dcache_range((uintptr_t)k3_fuse_buff, PAGE_SIZE);
+
 		ret = ti_sci_keywriter_lite((unsigned long)&k3_fuse_buff->payload);
 		if (ret) {
 			ERROR("Keywriter Lite Failed: (%d)\n", ret);