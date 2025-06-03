TexasInstruments/arm-trusted-firmware

commit a70f82d5a439cb9e21426908c7410c4f2a646364

PENDING: feat(ti): Add SiP SVC calls for fuse programming
Add Silicon Vendor specific SVC calls to read and write to general
purpose OTP fuses using TISCI calls underneath.

These fuses may contain user keys etc and thus better handled in
trusted environment (TF-A).

Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

plat/ti/k3/common/k3_svc.c
@@ -14,6 +14,7 @@
 #include <lib/mmio.h>
 #include <tools_share/uuid.h>
 #include <k3_sip_svc.h>
+#include <ti_sci.h>
 
 /* K3 SiP Service UUID */
 DEFINE_SVC_UUID2(ti_sip_svc_uid,
@@ -33,6 +34,8 @@ uintptr_t sip_smc_handler(uint32_t smc_fid,
 			  void *handle,
 			  u_register_t flags)
 {
+	int ret = SMC_UNK;
+
         VERBOSE("sim_smc handler smc_fid = %u\n", smc_fid);
         switch (smc_fid) {
 	case SIP_SVC_CALL_COUNT:
@@ -54,6 +57,37 @@ uintptr_t sip_smc_handler(uint32_t smc_fid,
 
 	case K3_SIP_OTP_WRITEBUFF:
 		SMC_RET1(handle, ti_fuse_writebuff_handler(x1));
+
+	case K3_SIP_OTP_READ:
+		uint32_t mmr_val = 0;
+
+		/*
+		 * 0x00 - 0xFE is reserved for user OTP, 0xFF is
+		 * reserved for bootmode OTP which doesnt support
+		 * readback
+		 */
+		if (x1 < 0xff)
+			ret = ti_sci_read_otp(x1, x2, &mmr_val);
+
+		SMC_RET2(handle, ret, mmr_val);
+
+	case K3_SIP_OTP_WRITE:
+		/*
+		 * 0x00 - 0xFE is reserved for user OTP, 0xFF is
+		 * reserved for bootmode OTP programming
+		 */
+		if (x1 < 0xff) {
+			uint32_t row_val = 0;
+
+			ret = ti_sci_write_otp(x1, x2, x3, x4, &row_val);
+
+			SMC_RET2(handle, ret, row_val);
+		}
+
+		if (x1 == 0xff)
+			SMC_RET2(handle, ti_sci_set_otp_bootmode(x2, x3), x3);
+
+		SMC_RET1(handle, SMC_UNK);
 
 	default:
 		ERROR("%s: unhandled SMC (0x%x)\n", __func__, smc_fid);

plat/ti/k3/include/k3_sip_svc.h
@@ -15,13 +15,15 @@
 #define SIP_SVC_VERSION         0x8200ff03
 #define K3_SIP_SCMI_AGENT0      0x82004000
 #define K3_SIP_OTP_WRITEBUFF    0xC2000000
+#define K3_SIP_OTP_WRITE        0xC2000001
+#define K3_SIP_OTP_READ         0xC2000002
 
 /* TI SiP Service Calls version numbers */
 #define K3_SIP_SVC_VERSION_MAJOR    0x0
 #define K3_SIP_SVC_VERSION_MINOR    0x1
 
 /* Number of TI SiP Calls implemented */
-#define K3_COMMON_SIP_NUM_CALLS     0x4
+#define K3_COMMON_SIP_NUM_CALLS     0x6
 
 /* TI Fuse writebuff SMC handler */
 int ti_fuse_writebuff_handler(u_register_t x1);

commit fdab60edb57339ebf10ce0d5bb98fd695061db18

PENDING: fix(ti): Fix compilation issues with GCC compiler version 9.2-2019.12.
Declare the variables "mmr_val"/"row_val" at the beginning of the
function instead of particular switch case labels to avoid compilation
issues with the toolchain "gcc-arm-9.2-2019.12-x86_64-aarch64-none-elf".

Fixes: a70f82d5a ("PENDING: feat(ti): Add SiP SVC calls for fuse programming")
Signed-off-by: Yash Srivastava <y-srivastava@ti.com>

plat/ti/k3/common/k3_svc.c
@@ -34,6 +34,8 @@ uintptr_t sip_smc_handler(uint32_t smc_fid,
 			  void *handle,
 			  u_register_t flags)
 {
+	uint32_t mmr_val = 0;
+	uint32_t row_val = 0;
 	int ret = SMC_UNK;
 
         VERBOSE("sim_smc handler smc_fid = %u\n", smc_fid);
@@ -59,8 +61,6 @@ uintptr_t sip_smc_handler(uint32_t smc_fid,
 		SMC_RET1(handle, ti_fuse_writebuff_handler(x1));
 
 	case K3_SIP_OTP_READ:
-		uint32_t mmr_val = 0;
-
 		/*
 		 * 0x00 - 0xFE is reserved for user OTP, 0xFF is
 		 * reserved for bootmode OTP which doesnt support
@@ -77,8 +77,6 @@ uintptr_t sip_smc_handler(uint32_t smc_fid,
 		 * reserved for bootmode OTP programming
 		 */
 		if (x1 < 0xff) {
-			uint32_t row_val = 0;
-
 			ret = ti_sci_write_otp(x1, x2, x3, x4, &row_val);
 
 			SMC_RET2(handle, ret, row_val);